63f3a3e2d09402112d9caf14d12ec2253c93bc59
k98a73 <100816862+k98a73@users.noreply.github.com>
2022-07-17 15:38:26 +0900

    #15 chore:文字色の追加

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,9 +4,9 @@ import { Flex, Heading, Spacer, Text } from "@chakra-ui/react"
 export const Header = () => {
   return (
     <Flex h="80px" bgColor="green.300" alignItems="center">
-      <Heading ml="99px" fontSize="48px">TODO</Heading>
+      <Heading color="blackAlpha.800" ml="99px" fontSize="48px">TODO</Heading>
       <Spacer />
-      <Text mr="100px" fontSize="16px">2022/01/01</Text>
+      <Text color="blackAlpha.800" mr="100px" fontSize="16px">2022/01/01</Text>
     </Flex>
   )
 }
