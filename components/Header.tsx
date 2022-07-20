e44c67c269e9a3c258b4e941d90c9a622c572a7f
k98a73 <100816862+k98a73@users.noreply.github.com>
2022-07-20 11:18:07 +0900

    #15 refactor: 文字を太字に修正

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,12 +1,21 @@
 import React from "react";
-import { Flex, Heading, Spacer, Text } from "@chakra-ui/react"
+import { Flex, Heading, Spacer, Text } from "@chakra-ui/react";
 
 export const Header = () => {
   return (
     <Flex h="80px" bgColor="green.300" alignItems="center">
-      <Heading color="blackAlpha.800" ml="99px" fontSize="48px">TODO</Heading>
+      <Heading
+        color="blackAlpha.800"
+        ml="99px"
+        fontSize="48px"
+        fontWeight="bold"
+      >
+        TODO
+      </Heading>
       <Spacer />
-      <Text color="blackAlpha.800" mr="100px" fontSize="16px">2022/01/01</Text>
+      <Text color="blackAlpha.800" mr="100px" fontSize="16px" fontWeight="bold">
+        2022/01/01
+      </Text>
     </Flex>
-  )
-}
+  );
+};
